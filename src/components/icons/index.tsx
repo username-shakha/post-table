931b733ae03221ea88ff2d32aab44348f5881602
username-shakha <username.shakha@gmail.com>
2024-05-01 20:11:11 +0500

    created icon components

---
+++ src/components/icons/index.tsx
@@ -0,0 +1,53 @@
+export function DownIcon() {
+  return (
+    <svg
+      width="12"
+      height="7"
+      viewBox="0 0 12 7"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <line
+        x1="0.353553"
+        y1="0.646447"
+        x2="6.18011"
+        y2="6.47301"
+        stroke="#FCFCFC"
+      />
+      <line
+        x1="5.64645"
+        y1="6.30331"
+        x2="11.3033"
+        y2="0.646453"
+        stroke="white"
+      />
+    </svg>
+  )
+}
+
+export function UpIcon() {
+  return (
+    <svg
+      width="12"
+      height="7"
+      viewBox="0 0 12 7"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <line
+        y1="-0.5"
+        x2="8.24"
+        y2="-0.5"
+        transform="matrix(0.707107 -0.707107 -0.707107 -0.707107 0 5.82655)"
+        stroke="#FCFCFC"
+      />
+      <line
+        y1="-0.5"
+        x2="8"
+        y2="-0.5"
+        transform="matrix(0.707107 0.707107 0.707107 -0.707107 6 0.169693)"
+        stroke="white"
+      />
+    </svg>
+  )
+}
