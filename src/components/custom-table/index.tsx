fd2e3a13d9ba8627e5e2faa00220e05493a28831
username-shakha <username.shakha@gmail.com>
2024-05-01 19:50:54 +0500

    created usePosts

--- src/components/custom-table/index.tsx
+++ src/components/custom-table/index.tsx
@@ -10,71 +10,50 @@ const defaultStyle: React.CSSProperties = {
 type TCustomTableProps = {
   heads: Array<IHeads>
   rows: Array<{ [key: string]: ReactNode }>
-  loading?: boolean
+  isLoading?: boolean
 }
 
 export default function CustomTable({
   heads,
   rows,
-  loading = false,
+  isLoading,
 }: TCustomTableProps) {
-  const headsNotEmpty = Array.isArray(heads) && heads.length >= 1
-  const rowsNotEmpty = Array.isArray(rows) && rows.length >= 1
   return (
-    !loading && (
-      <div>
-        <table>
-          <thead>
-            {headsNotEmpty ? (
-              <tr>
-                {heads.map((h) => (
-                  <th key={h.key} style={{ ...defaultStyle, ...h.style }}>
-                    {h.label}
-                  </th>
-                ))}
-              </tr>
-            ) : (
-              <tr>
-                <th className="error-notification">no column data available</th>
-              </tr>
-            )}
-          </thead>
-          <tbody>
-            {rowsNotEmpty ? (
-              rows.map((r, i) => (
-                <tr key={i}>
-                  {headsNotEmpty ? (
-                    heads.map((h) => (
-                      <td
-                        key={h.key}
-                        style={{
-                          ...defaultStyle,
-                          ...h.style,
-                        }}
-                      >
-                        {!r[h.key] ? `key mismatch: ${r[h.key]}` : r[h.key]}
-                      </td>
-                    ))
-                  ) : (
-                    <td>
-                      <td className="error-notification" colSpan={3}>
-                        The column data is not available and does not match the
-                        row data
-                      </td>
-                    </td>
-                  )}
-                </tr>
-              ))
-            ) : (
-              <tr>
-                <td className="error-notification" colSpan={heads.length ?? 20}>
-                  there is no data in the table
+    <table>
+      <thead>
+        <tr>
+          {heads.map((h) => (
+            <th key={h.key} style={{ ...defaultStyle, ...h.style }}>
+              {h.label}
+            </th>
+          ))}
+        </tr>
+      </thead>
+      <tbody>
+        {isLoading ? (
+          <tr>
+            <td className="loading" colSpan={heads.length}>
+              Loading....
+            </td>
+          </tr>
+        ) : (
+          rows.map((r, i) => (
+            <tr key={i}>
+              {heads.map((h) => (
+                <td
+                  key={h.key}
+                  style={{
+                    ...defaultStyle,
+                    ...h.style,
+                  }}
+                >
+                  {r[h.key]}
                 </td>
-              </tr>
-            )}
-          </tbody>
-        </table>
-      </div>
-    )
+              ))}
+            </tr>
+          ))
+        )}
+      </tbody>
+    </table>
   )
 }
---
+++ src/constants/index.tsx
@@ -0,0 +1,20 @@
+export const POSTS_TABLE_HEADS = [
+  {
+    key: 'id',
+    label: 'ID',
+    style: {
+      width: 100,
+      padding: 5,
+    },
+  },
+  {
+    key: 'title',
+    label: 'Title',
+    style: { width: 500, padding: 5 },
+  },
+  {
+    key: 'body',
+    label: 'Body',
+    style: { width: 400, padding: 5 },
+  },
+]
