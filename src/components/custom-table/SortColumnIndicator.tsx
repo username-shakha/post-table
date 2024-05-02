33ca92903841820c39690de74cf7b487c3c0896f
username-shakha <username.shakha@gmail.com>
2024-05-02 20:18:09 +0500

    created sortColumn indicator

---
+++ src/components/custom-table/SortColumnIndicator.tsx
@@ -0,0 +1,15 @@
+import { SortOption } from '../../interfaces'
+import { DownIcon, UpIcon } from '../icons'
+
+interface ISortColumnIndicatorProps {
+  sortType: SortOption | null
+}
+
+export default function SortColumnIndicator({
+  sortType,
+}: ISortColumnIndicatorProps) {
+  console.log(sortType)
+  if (sortType === SortOption.ASC) return <UpIcon />
+  if (sortType === SortOption.DESC) return <DownIcon />
+  return sortType
+}
--- src/components/custom-table/index.tsx
+++ src/components/custom-table/index.tsx
@@ -1,5 +1,7 @@
-import { ReactNode } from 'react'
+import { ReactNode, useState } from 'react'
 import './style.css'
+import { IHeads, ISort, SortOption } from '../../interfaces'
+import SortColumnIndicator from './SortColumnIndicator'
 
 const defaultStyle: React.CSSProperties = {
   width: 200,
@@ -11,20 +13,54 @@ type TCustomTableProps = {
   heads: Array<IHeads>
   rows: Array<{ [key: string]: ReactNode }>
   isLoading?: boolean
+  onSort?: (data: ISort | null) => void
 }
 
 export default function CustomTable({
   heads,
   rows,
   isLoading,
+  onSort,
 }: TCustomTableProps) {
+  const [sortType, setSortType] = useState<SortOption | null>(null)
+  const [sortedColumn, setSortedColumn] = useState('')
+  console.log(sortType)
+  console.log(sortedColumn)
   return (
     <table>
       <thead>
         <tr>
           {heads.map((h) => (
-            <th key={h.key} style={{ ...defaultStyle, ...h.style }}>
+            <th
+              onClick={() => {
+                if (h.sortable === false) return
+                let sort: SortOption | null = null
+
+                if (sortType == null) sort = SortOption.ASC
+                if (sortType === SortOption.ASC) sort = SortOption.DESC
+                if (sortType === SortOption.DESC) sort = null
+                onSort?.(
+                  sort != null
+                    ? {
+                        key: h.key,
+                        sort,
+                        type: h.type,
+                      }
+                    : null
+                )
+                setSortType(sort)
+                setSortedColumn(sort != null ? h.key : '')
+              }}
+              key={h.key}
+              style={{ ...defaultStyle, ...h.style }}
+            >
               {h.label}
+
+              {(h.sortable ?? true) && sortedColumn === h.key && (
+                <div style={{ marginLeft: 16, display: 'inline' }}>
+                  <SortColumnIndicator sortType={sortType} />
+                </div>
+              )}
             </th>
           ))}
         </tr>
