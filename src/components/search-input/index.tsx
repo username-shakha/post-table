f3748df323899430b6b3398ac5a55e7b7f9716b1
username-shakha <username.shakha@gmail.com>
2024-05-06 14:24:21 +0500

    fix search input placeholder

--- src/components/search-input/index.tsx
+++ src/components/search-input/index.tsx
@@ -5,18 +5,25 @@ interface ISearchInputProps extends React.HTMLAttributes<HTMLInputElement> {
   startIcon?: ReactNode
   endIcon?: ReactNode
   disabled?: boolean
+  placeholder: string
 }
 export const SearchInput = ({
   endIcon,
   startIcon,
   style,
   disabled,
+  placeholder,
   ...props
 }: ISearchInputProps) => {
   return (
     <div className="input-wrapper" style={style}>
       {startIcon && <div className="start icon">{startIcon}</div>}
-      <input disabled={disabled} type="text" {...props} />
+      <input
+        placeholder={placeholder}
+        disabled={disabled}
+        type="text"
+        {...props}
+      />
       {endIcon && <div className="end icon">{endIcon}</div>}
     </div>
   )
